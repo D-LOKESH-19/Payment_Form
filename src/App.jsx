975c87b91f7bd256f3d00635de77653e1ee66693
D Lokesh <lokeshreddy0096@gmail.com>
2026-03-14 21:04:41 +0530

    vedio  checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import Texter from "./Tester";
-// import VideoTester from "./videoTester";
+import Texter from "./Tester";
+import VideoTester from "./videoTester";
 import FireChecker from "./FireChecker";
 
 const App = () => {
   return (
     <>
-      {/* <Texter/>
-      <VideoTester/> */}
-      <FireChecker/>
+      <Texter/>
+      <VideoTester/>
+      {/* <FireChecker/> */}
     </>
   );
 };
